28afe7061a3096a77d2d67383e3f0e6ae66aa9ab
arthurklr_ <arthur.k2104@gmail.com>
2024-10-01 20:52:41 +0200

    displays all session variables

---
+++ page3.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+var_dump($_SESSION);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Page 3</title>
+</head>
+
+<body>
+  <a href="page1.php" title="Lien vers page1.php">Lien vers page1</a>
+  <a href="page2.php" title="Lien vers page2.php">Lien vers page2</a>
+  <a href="page4.php" title="Lien vers page4.php">Lien vers page4</a>
+</body>
+
+</html>
---
+++ page4.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+if (isset($_SESSION)) {
+    session_destroy();
+    echo "La session a été fermée";
+} else {
+    echo "Aucune session active à fermer";
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Page 4</title>
+</head>
+
+<body>
+  <a href="page1.php" title="Lien vers page1.php">Lien vers page1</a>
+  <a href="page2.php" title="Lien vers page2.php">Lien vers page2</a>
+  <a href="page4.php" title="Lien vers page4.php">Lien vers page4</a>
+</body>
+
+</html>
